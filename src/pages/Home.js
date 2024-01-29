d28bcd3d2c4f86cb4087135ca792405381fca567
Khush Vijayvarigya <93064601+Khush2021@users.noreply.github.com>
2024-01-29 12:14:27 +0530

    connected to backend through socket, global socket context created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,13 @@ import { Sidebar } from "../components/sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { getConversations } from "../features/chatSlice";
 import { ChatContainer, WhatsappHome } from "../components/chat";
+import SocketContext from "../context/SocketContext.js";
 
-const Home = () => {
+const Home = ({ socket }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const { activeConversation } = useSelector((state) => state.chat);
-  // console.log(activeConversation);
+  console.log(socket);
 
   //get conversations
   useEffect(() => {
@@ -28,4 +29,9 @@ const Home = () => {
   );
 };
 
-export default Home;
+const HomeWithSocket = (props) => (
+  <SocketContext.Consumer>
+    {(socket) => <Home {...props} socket={socket} />}
+  </SocketContext.Consumer>
+);
+export default HomeWithSocket;
